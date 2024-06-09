49c60a4a10c7111c38896c694d7ef03d9c8addce
Anton Rudnikovskiy <mkasadasa@yandex.ru>
2024-06-09 15:56:49 +0300

    zipkin

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,4 +12,7 @@ repositories {
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server:4.1.2")
     implementation("com.google.code.gson:gson:2.8.9")
+    implementation("org.springframework.boot:spring-boot-starter-actuator:3.1.5")
+    implementation("io.micrometer:micrometer-tracing-bridge-brave:1.0.2")
+    implementation("io.zipkin.reporter2:zipkin-reporter-brave:2.16.3")
 }
